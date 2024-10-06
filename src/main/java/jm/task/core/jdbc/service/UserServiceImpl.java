306d34bc7ffa686ef4a451628f95e86cd14754f3
GTilt <perrrshin@vk.com>
2024-10-06 18:29:23 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+/*
+Соединение
+логику че сделать
+отправить то что сделали
+закрыть соединение
+*/
+
+public class UserServiceImpl implements UserService {
+    public void createUsersTable() {
+
+    }
+
+    public void dropUsersTable() {
+
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+
+    }
+
+    public void removeUserById(long id) {
+
+    }
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public void cleanUsersTable() {
+
+    }
+}

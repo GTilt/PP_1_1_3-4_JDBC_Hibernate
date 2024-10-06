50d35a2c9079a75cfee5a949d4d8a01db4f195a3
GTilt <perrrshin@vk.com>
2024-10-07 01:54:35 +0300

    Done Test

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -38,7 +38,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try (Connection newConnection = getNewConnection();
-             PreparedStatement preparedStatement = newConnection.prepareStatement("INSERT INTO Users (name, lastName, age) VALUES (?, ?, ?) ");) {
+             PreparedStatement preparedStatement = newConnection.prepareStatement("INSERT INTO Users (name, lastName, age) VALUES (?, ?, ?) ")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);

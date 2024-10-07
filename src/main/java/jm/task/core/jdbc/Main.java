893a15c2c778e175132e3be8819dd240c3df6ee7
tilt <perrrshin@vk.com>
2024-10-07 13:32:06 +0300

    fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,26 +1,21 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDao userDao = new UserDaoJDBCImpl();
-        try {
-            userDao.createUsersTable();
-            userDao.saveUser("Sergey", "Pershin", (byte) 22);
-            userDao.saveUser("Ilon", "Mask", (byte)30);
-            userDao.saveUser("Steve", "Jobs", (byte)67);
-            userDao.saveUser("Ana", "D'Armas", (byte)28);
-            userDao.getAllUsers().toString();
-            userDao.cleanUsersTable();
-            userDao.dropUsersTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Sergey", "Pershin", (byte) 22);
+        userService.saveUser("Ilon", "Mask", (byte) 30);
+        userService.saveUser("Steve", "Jobs", (byte) 67);
+        userService.saveUser("Ana", "D'Armas", (byte) 28);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
+
+
